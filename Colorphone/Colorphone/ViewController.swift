4474246a9e19ec2dd4f7fc94eacddf59989554df
Eugene Berezin <berezin.nlp@gmail.com>
2019-07-05 11:53:30 -0700

    Creating colorful cells

---
+++ Colorphone/Colorphone/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Colorphone
+//
+//  Created by Eugene Berezin on 7/5/19.
+//  Copyright © 2019 Eugene Berezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let numViewPerRow = 15
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        
+        for j in 0...30 {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
+            }
+        }
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        print(location)
+    }
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+}
